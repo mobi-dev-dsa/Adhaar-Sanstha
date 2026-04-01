1504c1fcaf4e2b7c335b5b9e63019bb7cb941b29
lade6501 <lade6501@gmail.com>
2026-04-01 15:10:17 +0530

    chore : removed extra space

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   supabaseUrl: 'https://ecdzftgynewgsfsyiouj.supabase.co',
   supabaseKey:
-    ' eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjZHpmdGd5bmV3Z3Nmc3lpb3VqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxNjM4ODMsImV4[cCI6MjA2OTczOTg4M30.cbg1eF-8tKBHM5c1GFEaZsunukGkApGr5PkDmU3d1vg',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjZHpmdGd5bmV3Z3Nmc3lpb3VqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxNjM4ODMsImV4[cCI6MjA2OTczOTg4M30.cbg1eF-8tKBHM5c1GFEaZsunukGkApGr5PkDmU3d1vg',
   recaptcha: {
     siteKey: '6Lc-qZgrAAAAAJeYEXZWbmvqZ-nMLBR9go53rBZS',
   },
